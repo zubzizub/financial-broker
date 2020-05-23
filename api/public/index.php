7ad8879e9e09d5a48dca83dd47eaaad4238684e6
Alexey Zubkov <zubzizub85@gmail.com>
2020-05-23 17:50:22 +0200

    Added error reporting

--- api/public/index.php
+++ api/public/index.php
@@ -1,14 +1,18 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 use Slim\Factory\AppFactory;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
+http_response_code(500);
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+
+$app->addErrorMiddleware(false, true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write('{hello!}');
